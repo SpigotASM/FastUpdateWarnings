7d17083875feccea1bc968c064b6914a893500b1
Dan <cyberpwnn@users.noreply.github.com>
2017-11-18 08:18:33 -0500

    kdun

---
+++ src/main/java/org/cyberpwn/fuw/FUW.java
@@ -0,0 +1,8 @@
+package org.cyberpwn.fuw;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FUW extends JavaPlugin
+{
+
+}
---
+++ src/main/java/org/cyberpwn/fuw/transformers/AntiSleepTransformer.java
@@ -0,0 +1,41 @@
+package org.cyberpwn.fuw.transformers;
+
+import org.cyberpwn.classtweaker.IClassTransformer;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+
+public class AntiSleepTransformer implements IClassTransformer
+{
+	public byte[] transform(String className, byte[] bytes)
+	{
+		ClassNode node = ASM.readNode(bytes);
+
+		searching: for(Object i : node.methods)
+		{
+			MethodNode m = (MethodNode) i;
+
+			if(m.name.equals("main"))
+			{
+				for(AbstractInsnNode j : m.instructions.toArray())
+				{
+					if(j.getOpcode() == Opcodes.GETSTATIC)
+					{
+						FieldInsnNode f = (FieldInsnNode) j;
+
+						if(f.owner.equals("java/util/concurrent/TimeUnit") && f.desc.equals("Ljava/util/concurrent/TimeUnit;") && f.name.equals("SECONDS"))
+						{
+							f.name = "MILLISECONDS";
+
+							break searching;
+						}
+					}
+				}
+			}
+		}
+
+		return ASM.writeNode(node);
+	}
+}
